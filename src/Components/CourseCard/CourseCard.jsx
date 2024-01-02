7869f273b16b46b306c7947b71a92fff7bac9ade
Manoj.KraysInfoTech <manoj.kraysinfotech@gmail.com>
2024-01-02 12:47:32 +0530

    resolve UI bugs

--- src/Components/CourseCard/CourseCard.jsx
+++ src/Components/CourseCard/CourseCard.jsx
@@ -48,8 +48,8 @@ const CourseCard = ({ course, path }) => {
           <div className="courseImgWrapper h-36 overflow-hidden rounded-lg">
             <img
               // src={`data:image/jpeg;base64,${course.thumbNail}`}
-              // src={`${course.thumbNail}`}
-              src={isTokenValid ? `${course.thumbNail}` : course.img}
+              src={`${course.thumbNail}`}
+              // src={isTokenValid ? `${course.thumbNail}` : course.thumbNail}
               alt="course thumbnail"
               className="courseImg h-full w-full object-cover"
             />
