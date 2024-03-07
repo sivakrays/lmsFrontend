8ef05e4111e5e496d7b8b946fb97a3aa3bb3ba90
Manoj.KraysInfoTech <manoj.kraysinfotech@gmail.com>
2024-03-07 18:13:43 +0530

    implement the draggable widget functionality

--- src/Dashboard/DragableWidget/DragableWidget.jsx
+++ src/Dashboard/DragableWidget/DragableWidget.jsx
@@ -4,6 +4,7 @@ import Preview from "./Preview";
 
 import { FaArrowTurnDown } from "react-icons/fa6";
 import { FaArrowTurnUp } from "react-icons/fa6";
+import { post } from "../../ApiCall/ApiCall";
 
 const DragableWidget = () => {
   const [widgets, setWidgets] = useState([]);
@@ -11,9 +12,19 @@ const DragableWidget = () => {
   const [imageFiles, setImageFiles] = useState([]);
   const [videoFiles, setVideoFiles] = useState([]);
 
+  // vidoe upload states
+  const [uploading, setUploading] = useState(false);
+  const [video, setVideo] = useState();
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const currentToken = JSON.parse(localStorage.getItem("token"));
+
+  // handle Drop functionality
+
   const handleOnDrag = (e, type) => {
     e.dataTransfer.setData("type", type);
   };
+
+  // Handle drop functionality
 
   const handleOnDrop = (e, targetIndex) => {
     e.preventDefault();
@@ -30,9 +41,13 @@ const DragableWidget = () => {
     }
   };
 
+  // dragOver the widget functionality
+
   const handleDragOver = (e) => {
     e.preventDefault();
   };
+
+  // remove widget functionality
 
   const removeWidget = (index) => {
     const newWidgets = [...widgets];
@@ -52,11 +67,15 @@ const DragableWidget = () => {
     setVideoFiles(newVideoFiles);
   };
 
+  // Editor input change functionality
+
   const handleEditorChange = (content, index) => {
     const newEditorContents = [...editorContents];
     newEditorContents[index] = content;
     setEditorContents(newEditorContents);
   };
+
+  // Image change Functionality
 
   const handleImageChange = (file, index) => {
     if (!file) return; // If no file is selected, do nothing
@@ -70,6 +89,8 @@ const DragableWidget = () => {
     };
   };
 
+  // Video Change Functionality
+
   const handleVideoChange = (file, index) => {
     if (!file) return; // If no file is selected, do nothing
     const reader = new FileReader();
@@ -82,29 +103,7 @@ const DragableWidget = () => {
     };
   };
 
-  const handlePublish = () => {
-    const editorData = widgets.map((widget, index) => {
-      if (widget === "widget A") {
-        return {
-          type: "editor",
-          content: editorContents[index],
-        };
-      } else if (widget === "widget B") {
-        return {
-          type: "video",
-          content: videoFiles[index],
-        };
-      } else if (widget === "widget C") {
-        return {
-          type: "image",
-          content: imageFiles[index],
-        };
-      }
-      return null;
-    });
-
-    console.log("Editor Data", editorData);
-  };
+  // Widget up down Moving Functionality
 
   const moveWidget = (currentIndex, direction) => {
     const newIndex = direction === "up" ? currentIndex - 1 : currentIndex + 1;
@@ -129,9 +128,84 @@ const DragableWidget = () => {
     setVideoFiles(newVideoFiles);
   };
 
+  // Publish the page Functionality
+
+  const handlePublish = () => {
+    const widgetData = widgets.map((widget, index) => {
+      if (widget === "widget A") {
+        return {
+          type: "editor",
+          content: editorContents[index],
+        };
+      } else if (widget === "widget B") {
+        return {
+          type: "video",
+          content: videoFiles[index],
+        };
+      } else if (widget === "widget C") {
+        return {
+          type: "image",
+          content: imageFiles[index],
+        };
+      } else if (widget === "widget D") {
+        return {
+          type: "image&Content",
+          image: imageFiles[index],
+          content: editorContents[index],
+        };
+      }
+      return null;
+    });
+
+    console.log("Widget Data", widgetData);
+  };
+
+  //Video Upload Functionality
+
+  const handleVideoUpload = async (index) => {
+    setUploading(true);
+
+    const formData = new FormData();
+    formData.append("file", video);
+
+    try {
+      const res = await post(`/user/uploadFile?courseId=${"test"}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${currentToken}`,
+        },
+        onUploadProgress: (progressEvent) => {
+          const { loaded, total } = progressEvent;
+          const percentCompleted = Math.round((loaded * 100) / total);
+          setUploadProgress(percentCompleted);
+        },
+      });
+
+      console.log(res.data);
+      const newVideoFiles = [...videoFiles];
+      newVideoFiles[index] = res.data;
+      setVideoFiles(newVideoFiles);
+    } catch (error) {
+      console.error("Error uploading video:", error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  //change the order in image&content widget
+
+  const [contentFirst, setContentFirst] = useState(false);
+  const [clickedIndex, setClickedIndex] = useState("");
+
+  const handleChangeOrder = (index) => {
+    setContentFirst(!contentFirst);
+    console.log(index, contentFirst);
+  };
+
   return (
     <>
       <div>
+        {/* Widget ToolBar */}
         <div className="widgets flex gap-3">
           <div
             className="widget border"
@@ -154,8 +228,16 @@ const DragableWidget = () => {
           >
             Image
           </div>
+          <div
+            className="widget border"
+            draggable
+            onDragStart={(e) => handleOnDrag(e, "widget D")}
+          >
+            Image + content
+          </div>
         </div>
 
+        {/* Widgets */}
         <div
           className="page min-h-screen w-full space-y-3 border"
           onDrop={(e) => handleOnDrop(e, widgets.length)}
@@ -175,22 +257,44 @@ const DragableWidget = () => {
                     onChange={(content) => handleEditorChange(content, index)}
                   />
                 )}
+
                 {widget === "widget B" && (
-                  <div className="flex gap-3">
-                    <label htmlFor="file-input" className="sr-only">
-                      Choose file
-                    </label>
-                    <input
-                      type="file"
-                      name="video"
-                      id="video"
-                      className="focus:textColor focus:border-grey-200 block w-full rounded-lg border border-gray-200 text-sm shadow-sm file:me-4 file:border-0 file:bg-gray-50 file:px-4 file:py-3 focus:z-10 disabled:pointer-events-none disabled:opacity-50 "
-                      onChange={(e) =>
-                        handleVideoChange(e.target.files[0], index)
-                      }
-                    />
-                  </div>
+                  <>
+                    <div className="flex gap-3">
+                      <label htmlFor="file-input" className="sr-only">
+                        Choose file
+                      </label>
+                      <input
+                        type="file"
+                        name="video"
+                        id="video"
+                        className="focus:textColor focus:border-grey-200 block w-full rounded-lg border border-gray-200 text-sm shadow-sm file:me-4 file:border-0 file:bg-gray-50 file:px-4 file:py-3 focus:z-10 disabled:pointer-events-none disabled:opacity-50 "
+                        onChange={(e) => {
+                          // handleVideoChange(e.target.files[0], index)
+                          setVideo(e.target.files[0]);
+                        }}
+                      />
+                      <button
+                        className="border-grey-400 rounded-md border px-3"
+                        type="button"
+                        onClick={() => handleVideoUpload(index)}
+                      >
+                        Upload
+                      </button>
+                    </div>
+                    {uploading && (
+                      <div className="progress-container">
+                        <div
+                          className="progress-bar"
+                          style={{ width: `${uploadProgress}%` }}
+                        >
+                          {uploadProgress}%
+                        </div>
+                      </div>
+                    )}
+                  </>
                 )}
+
                 {widget === "widget C" && (
                   <>
                     <div className="flex gap-3">
@@ -225,7 +329,68 @@ const DragableWidget = () => {
                     </div>
                   </>
                 )}
+
+                {widget === "widget D" && (
+                  <>
+                    <button
+                      type="button"
+                      className="mb-2 rounded-md bg-gray-500 px-2 py-1.5 text-white"
+                      onClick={() => handleChangeOrder(index)}
+                    >
+                      Change Order
+                    </button>
+                    <div
+                      className={`flex flex-wrap ${
+                        contentFirst === true && "flex-row-reverse"
+                      }`}
+                    >
+                      <div className="min-h-[200px] w-1/2 border">
+                        <>
+                          <div className="flex gap-3">
+                            {imageFiles[index] ? (
+                              <img
+                                src={imageFiles[index]}
+                                alt={`Image ${index}`}
+                                style={{
+                                  width: "50%",
+                                  height: "50%",
+                                  marginLeft: "auto",
+                                  marginRight: "auto",
+                                }}
+                              />
+                            ) : (
+                              <>
+                                <label htmlFor="file-input" className="sr-only">
+                                  Choose file
+                                </label>
+                                <input
+                                  type="file"
+                                  name="image"
+                                  id="image"
+                                  className="focus:textColor focus:border-grey-200 block w-full rounded-lg border border-gray-200 text-sm shadow-sm file:me-4 file:border-0 file:bg-gray-50 file:px-4 file:py-3 focus:z-10 disabled:pointer-events-none disabled:opacity-50 "
+                                  onChange={(e) =>
+                                    handleImageChange(e.target.files[0], index)
+                                  }
+                                />
+                              </>
+                            )}
+                          </div>
+                        </>
+                      </div>
+                      <div className="min-h-[200px] w-1/2 border">
+                        <JoditEditor
+                          value={editorContents[index] || ""}
+                          onChange={(content) =>
+                            handleEditorChange(content, index)
+                          }
+                        />
+                      </div>
+                    </div>
+                  </>
+                )}
+
                 <div className="flex gap-2">
+                  {/*Position Button*/}
                   {index > 0 && (
                     <button
                       onClick={() => moveWidget(index, "up")}
@@ -244,6 +409,9 @@ const DragableWidget = () => {
                       <FaArrowTurnDown />
                     </button>
                   )}
+
+                  {/* Remove Button */}
+
                   <button
                     className="my-2 flex w-full items-end justify-end rounded-md   px-2  py-1 "
                     onClick={() => removeWidget(index)}
@@ -269,6 +437,8 @@ const DragableWidget = () => {
           })}
         </div>
       </div>
+
+      {/*Publish Button*/}
       <button
         className="my-5 w-[200px] rounded-md bg-textLightColor px-2 py-1.5 text-white"
         type="button"
@@ -276,14 +446,15 @@ const DragableWidget = () => {
       >
         Publish
       </button>
-      {/*
+
+      {/*Preview the page*/}
       <Preview
         widgets={widgets}
         videoFiles={videoFiles}
         editorContents={editorContents}
         imageFiles={imageFiles}
       />
-      */}
+      {/**/}
     </>
   );
 };
--- src/Dashboard/DragableWidget/Preview.jsx
+++ src/Dashboard/DragableWidget/Preview.jsx
@@ -9,26 +9,33 @@ const Preview = ({ widgets, editorContents, imageFiles, videoFiles }) => {
         {widgets.map((widget, index) => {
           if (widget === "widget A") {
             return (
-              <div className="preview-widget" key={index}>
-                <h3>Editor Content</h3>
+              <div className="preview-widget px-14" key={index}>
+                {/* <h3>Editor Content</h3> */}
                 <div
                   dangerouslySetInnerHTML={{ __html: editorContents[index] }}
-                ></div>
+                />
               </div>
             );
           } else if (widget === "widget B") {
             return (
-              <div className="preview-widget" key={index}>
-                <h3>Video Preview</h3>
-                <video controls>
-                  <source src={videoFiles[index]} type="video/mp4" />
+              <div
+                className="preview-widget flex w-full items-center justify-center p-2"
+                key={index}
+              >
+                {/* <h3>Video Preview</h3> */}
+                <video
+                  controls
+                  src={videoFiles[index]}
+                  style={{ width: "50%", height: "50%" }}
+                >
+                  {/* <source src={videoFiles[index]} type="video/mp4" /> */}
                 </video>
               </div>
             );
           } else if (widget === "widget C") {
             return (
               <div className="preview-widget" key={index}>
-                <h3>Image Preview</h3>
+                {/* <h3>Image Preview</h3> */}
                 <img
                   src={imageFiles[index]}
                   alt={`Image ${index}`}
@@ -40,6 +47,25 @@ const Preview = ({ widgets, editorContents, imageFiles, videoFiles }) => {
                     justifyContent: "center",
                   }}
                 />
+              </div>
+            );
+          } else if (widget === "widget D") {
+            return (
+              <div className="preview-widget" key={index}>
+                {/* <h3>Image + Content Preview</h3> */}
+                <div className="image-content-preview flex">
+                  <div className="flex  w-1/2 flex-wrap items-center justify-center p-2">
+                    <img
+                      src={imageFiles[index]}
+                      alt={`Image ${index}`}
+                      style={{ width: "80%", height: "80%" }}
+                    />
+                  </div>
+                  <div
+                    className="flex w-1/2  flex-col items-center justify-center p-2"
+                    dangerouslySetInnerHTML={{ __html: editorContents[index] }}
+                  />
+                </div>
               </div>
             );
           }
